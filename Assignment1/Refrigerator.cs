17e7b455dbc682fb64d6c8132003c4be26cdcc15
Savanna Piscitelli <savannapiscitelli@gmail.com>
2023-02-08 09:47:07 -0700

    Add ToString methods

--- Assignment1/Refrigerator.cs
+++ Assignment1/Refrigerator.cs
@@ -6,22 +6,31 @@ using System.Threading.Tasks;
 
 namespace Assignment1
 {
-    internal class Refrigerator : Appliance
+    class Refrigerator : Appliance
     {
         private int numberOfDoors;
         private double height;
         private double width;
 
-        public Refrigerator(long itemNumber, string brand, int quantity, double wattage, string color, double price, int numberOfDoors, double height, double width) : base(itemNumber, brand, quantity, wattage, color, price)
+        public Refrigerator(long itemNumber, string brand, int quantity, double wattage, string color, double price, 
+            int numberOfDoors, double height, double width) : base(itemNumber, brand, quantity, wattage, color, price)
         {
             this.numberOfDoors = numberOfDoors;
             this.height = height;
             this.width = width;
         }
 
+
+
         public override string formatForFile()
         {
             return base.formatForFile() + this.numberOfDoors + ";" + this.height + ";" + this.width;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "\nNumber of Doors: " + this.numberOfDoors + "\nHeight: " + this.height + 
+                "\nWidth: " + this.width;
         }
     }
 }
