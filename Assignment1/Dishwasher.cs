c0bdad2b7e063b72b20ec6c5d4b0104f1bac9b17
jonahscott <119908106+jonahscott@users.noreply.github.com>
2023-02-11 01:15:32 -0800

    Update Dishwasher.cs

--- Assignment1/Dishwasher.cs
+++ Assignment1/Dishwasher.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Assignment1
 {
-    internal class Dishwasher
+   internal class Dishwasher : Appliance
     {
+        private string feature;
+        private string soundRating;
+
+        public Dishwasher(long itemNumber, string brand, int quantity, double wattage, string color, double price, 
+            string feature, string soundRating) : base(itemNumber, brand, quantity, wattage, color, price)
+        {
+            this.Feature = feature;
+            this.SoundRating = soundRating;
+        }
+
+        public string Feature { get; set; }
+        public string SoundRating { get; set; }
+
+        public override string formatForFile()
+        {
+            return base.formatForFile() + this.feature + ";" + this.soundRating;
+        }
+
+        public override string ToString()
+        {
+            return "Item Number: " + this.ItemNumber + "\nBrand: " + this.Brand + "\nQuantity: " + this.Quantity + 
+                "\nWattage: " + this.Wattage + "\nColor: " + this.Color + "\nPrice: " + this.Price + 
+                "\nFeature: " + this.feature + "\nSound Rating: " + this.soundRating + "\n";
+        }
     }
 }
+
