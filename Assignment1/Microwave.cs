7621f92d36d52b1ac23948193f012e616ce513b7
jonahscott <119908106+jonahscott@users.noreply.github.com>
2023-02-11 01:14:10 -0800

    Update Microwave.cs

--- Assignment1/Microwave.cs
+++ Assignment1/Microwave.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Assignment1
 {
-    internal class Microwave
+    internal class Microwave : Appliance
     {
+        private double capacity;
+        private string roomType;
+
+        public Microwave(long itemNumber, string brand, int quantity, double wattage, string color, double price, 
+            double capacity, string roomType) : base(itemNumber, brand, quantity, wattage, color, price)
+        {
+            this.Capacity = capacity;
+            this.RoomType = roomType;
+        }
+
+        public double Capacity { get; set; }
+        public string RoomType { get; set; }
+
+        public override string formatForFile()
+        {
+            return base.formatForFile() + this.capacity + ";" + this.roomType;
+        }
+
+        public override string ToString()
+        {
+            return "Item Number: " + this.ItemNumber + "\nBrand: " + this.Brand + "\nQuantity: " + this.Quantity + 
+                "\nWattage: " + this.Wattage + "\nColor: " + this.Color + "\nPrice: " + this.Price + 
+                "\nCapacity: " + this.capacity + "\nRoom Type: " + this.roomType + "\n";
+        }
     }
 }
+
